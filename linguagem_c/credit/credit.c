4af72bdc189a925a662d2bcedee57216738e7204
DfrancaEsl <dfranca@eslsistemas.com.br>
2026-01-27 16:53:30 -0300

    Validação dos cartoes, recebber o número e validar o mesmo

--- linguagem_c/credit/credit.c
+++ linguagem_c/credit/credit.c
@@ -1,21 +1,88 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char* validate_credit_card(long long card_number);
 
 int main(){
-  long long card_number = 4003600000000014;
-  int pick_number = 0;
+  char input[32];
+  long long card_number;
+  int valido;
 
-  while(card_number > 0){
-    int digit = card_number % 10;
+  do {
+    valido = 1;
+
+    printf("Number: ");
+    scanf("%31s", input);
+
+    for (int i = 0; input[i] != '\0'; i++) {
+      if (!isdigit(input[i])) {
+        valido = 0;
+        break;
+      }
+    }
+
+    if (valido) {
+      sscanf(input, "%lld", &card_number);
+    }
+  } while (!valido);
+
+
+  printf("%s\n", validate_credit_card(card_number));
+
+  return 0;
+}
+
+char* validate_credit_card(long long card_number){
+  int pick_number = 0;
+  int sum = 0;
+
+  long long number = card_number;
+
+  while(number > 0){
+    int digit = number % 10;
 
       if (pick_number)
       {
-        printf("%d\n", digit);
+        digit *= 2;
+        if (digit > 9){
+          digit -= 9;
+        }
       }
 
-    card_number /= 10;
+    sum += digit;
+    number /= 10;
     pick_number = !pick_number;
   }
 
+  if(sum % 10 != 0){
+    return "INVALID";
+  }
 
-  return 0;
+  long long starts = card_number;
+
+  while (starts >= 100){
+    starts /= 10;
+  }
+
+  if (starts == 34 || starts == 37){
+    return "AMEX";
+  }
+  else if (starts == 51 || starts == 52 || starts == 53 || starts == 54 || starts == 55){
+    return "MASTERCARD";
+  }
+  else
+  {
+    while (starts >= 10){
+      starts /= 10;
+    }
+
+    if (starts == 4){
+      return "VISA";
+    }
+    else{
+      return "INVALID";
+    }
+  }
+
 }
