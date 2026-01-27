45ffe5ab2c454419640ea25d097bf4cb76eec5d1
DfrancaEsl <dfranca@eslsistemas.com.br>
2026-01-27 15:35:47 -0300

    Pegar números alternados

--- linguagem_c/credit/credit.c
+++ linguagem_c/credit/credit.c
@@ -2,13 +2,18 @@
 
 int main(){
   long long card_number = 4003600000000014;
+  int pick_number = 0;
 
   while(card_number > 0){
     int digit = card_number % 10;
 
-    printf("%d\n", digit);
+      if (pick_number)
+      {
+        printf("%d\n", digit);
+      }
 
     card_number /= 10;
+    pick_number = !pick_number;
   }
 
 
