765ee893ff1e1a497fa01e1e1aa9c12d82b20016
danielfransa <daniel_fransa@hotmail.com>
2026-01-26 21:53:48 -0300

    Contrução da piramide

--- linguagem_c/mario-less/mario.c
+++ linguagem_c/mario-less/mario.c
@@ -9,7 +9,18 @@ int main(){
     scanf("%d", &h);
   } while (h<1 || h >8);
 
-  printf("Height value is: %d\n", h);
+  for (int i = 0; i < h; i++)
+  {
+    for (int j = h-1; j > i; j--)
+    {
+      printf(" ");
+    }
+    for (int k = 0; k < i+1; k++)
+    {
+      printf("#");
+    }
+    printf("\n");
+  }
 
   return 0;
 }
